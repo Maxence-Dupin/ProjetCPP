68fdd5d377546051fe120ea2b6bb35f1eecca256
Antirion <73186199+Antirion779@users.noreply.github.com>
2021-11-10 14:37:17 +0100

    fix reload files

--- ProjetCPP/ProjetCPP/Player.cpp
+++ ProjetCPP/ProjetCPP/Player.cpp
@@ -21,12 +21,15 @@ struct Player
 };
 
 
-void SetUpPlayer(Player& player, int radius, float speed, int hp, int shield)
+void SetUpPlayer(Player& player, float radius, float speed, int hp, int shield)
 {
-	player.circle.setRadius(radius);
 	player.speed = speed;
 	player.hp = hp;
 	player.shield = shield;
+	player.radius = radius;
+	player.circle.setPosition(sf::Vector2f(player.pos._x, player.pos._y));
+	player.circle.setRadius(player.radius);
+	player.circle.setFillColor(player.color);
 }
 
 
@@ -59,50 +62,4 @@ void ChangeShield(Player& player, int amout)
 	player.shield += amout;
 }
 
-
-#pragma once
-#include <iostream>
-#include <SFML/Graphics.hpp>
-#include "Player.hpp"
-
-void SetUpPlayer(Player& player, float radius, float speed, int hp, int shield)
-{
-	player.speed = speed;
-	player.hp = hp;
-	player.shield = shield;
-	player.radius = radius;
-	player.circle.setPosition(sf::Vector2f(player.pos._x, player.pos._y));
-	player.circle.setRadius(player.radius);
-	player.circle.setFillColor(player.color);
-}
-
-
-// Gestion des d�placement d'un rectangle
-void PlayerMouvement(Player& player, float deltaTime)
-{
-	float speed = player.speed; // 300 pixels par seconde
-
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::Z))//up
-		player.circle.move(sf::Vector2f(0.f, -speed * deltaTime));
-
-
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::S))//down
-		player.circle.move(sf::Vector2f(0.f, speed * deltaTime));
-
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::Q))//right
-		player.circle.move(sf::Vector2f(-speed * deltaTime, 0.f));
-
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D))//left
-		player.circle.move(sf::Vector2f(speed * deltaTime, 0.f));
-}
-
-void ChangeLife(Player& player, int amout)
-{
-	player.hp += amout;
-}
-
-void ChangeShield(Player& player, int amout)
-{
-	player.shield += amout;
-}
 
